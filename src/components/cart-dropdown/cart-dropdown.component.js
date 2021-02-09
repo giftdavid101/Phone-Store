a1fce8c818195a5c3744c3a852bd657ce9e95357
giftdavid101 <giftdavid101@gmail.com>
2021-02-09 20:06:45 +0100

    updated with reselect

--- src/components/cart-dropdown/cart-dropdown.component.js
+++ src/components/cart-dropdown/cart-dropdown.component.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import CustomButton from '../custom-button/custom-button.component'
 import './cart-dropdown.style.scss'
+import CartItem from '../cart-item/cart-item.component'
+import {connect} from 'react-redux'
+import {selectCartItems, selectCartItemsCount} from '../../redux/cart/cart.selectors'
 
 const CartDropdown = () => (
     <div className='cart-dropdown'>
-    <div className='cart-items'/>
+    <div className='cart-items'> 
+    {
+        cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem}/>)
+    }
+    </div>
     <CustomButton>Checkout</CustomButton>
     </div>
 )
-
-export default CartDropdown;
+const mapStateToProps = (state)=> ({
+    cartItems: selectCartItems(state)
+})
+export default connect(null, mapStateToProps)(CartDropdown) ;
--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,4 +1,5 @@
 CartActionTypes = {
     TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
+    ADD_ITEM:'ADD_ITEM'
 };
 export default CartActionTypes;
